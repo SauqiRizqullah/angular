5c7ce7c1be85c7cc8c7e51601a16d4043f03f6db
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-01-08 17:16:39 +0700

    [ADD] Component & Pengenalan Struktur Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // gak semua komponen pakai schema (Swiper slider)
 })
 export class AppComponent {
   title = 'angular-tutorial';
