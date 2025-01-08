b5b9b5a7e780d5a737fefb59e0858318322c9cbc
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-01-08 17:51:51 +0700

    [ADD] Components, Routes, Header Component (PART 1)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { PortofolioWebComponent } from './template/portofolio-web/portofolio-web.component';
+import { AdminComponent } from './template/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      // https://facebook.com/segment1/segment2
+      path: '',
+      component: PortofolioWebComponent // yang dipanggil nama class yang diexport typesrcipt
+    },
+    {
+      path: 'admin',
+      component: AdminComponent,
+    }
+]; // tempat ganti halaman
+
+// localhost/admin/dashboard
---
+++ src/app/components/portofolio-web/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header', // dicopy ke main portofolio htmlnya
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
