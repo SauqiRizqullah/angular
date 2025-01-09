e92a56c2fca45a96b5bf1619543d39b8fb96039c
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-01-09 11:34:41 +0700

    [ADD] Fixing Bugs, Path Alias, *ngFor, @for

--- src/app/components/portofolio-web/banner/banner.component.ts
+++ src/app/components/portofolio-web/banner/banner.component.ts
@@ -1,15 +1,21 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
   imports: [
     MatButtonModule,
+    NgFor
   ],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class BannerComponent {
-
+  listImages = [
+    "assets/images/bg-1.jpg",
+    "assets/images/bg-2.jpg",
+    "assets/images/bg-3.jpg"
+  ]
 }
