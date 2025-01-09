324d121d0b1ad3ede2f2fd22f6814ee630291f0d
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2025-01-09 09:32:15 +0700

    [ADD] Menu Bar, Banner, Swiper JS (Animasi Gambar)

---
+++ src/app/components/portofolio-web/banner/banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-banner',
+  imports: [
+    MatButtonModule,
+  ],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class BannerComponent {
+
+}
--- src/app/template/portofolio-web/portofolio-web.component.ts
+++ src/app/template/portofolio-web/portofolio-web.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/portofolio-web/header/header.component';
-
+import { BannerComponent } from '../../components/portofolio-web/banner/banner.component';
+import { HeaderMenuComponent } from '../../components/portofolio-web/header-menu/header-menu.component';
 
 @Component({
   selector: 'app-portofolio-web',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, BannerComponent, HeaderMenuComponent],
   templateUrl: './portofolio-web.component.html',
   styleUrl: './portofolio-web.component.css'
 })
